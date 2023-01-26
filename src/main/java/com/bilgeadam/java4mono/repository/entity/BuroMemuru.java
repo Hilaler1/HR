892a050e0e042a7d27ea5f45c4ac206c6af9aefe
Tuna Dağ <tunadag@gmail.com>
2023-01-26 13:25:28 +0300

    tuna deneme1325

--- src/main/java/com/bilgeadam/java4mono/repository/entity/BuroMemuru.java
+++ src/main/java/com/bilgeadam/java4mono/repository/entity/BuroMemuru.java
@@ -1,6 +1,9 @@
+
 package com.bilgeadam.java4mono.repository.entity;
 
 public class BuroMemuru {
     int kaman;
     int kırşehir;
+    // Deneme için yorum satırı eklendi.
+    String tunaDeneme;
 }
